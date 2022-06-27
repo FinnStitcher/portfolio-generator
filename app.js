b279e84c1f4c4ca504dbc74849b20527f86ebd71
Finn Stitcher <foundfootage@protonmail.com>
2022-06-27 16:18:22 -0700

    generate html

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 // take everything from index 2 to the end and put it in profileDatArgs
 // if i run node app.js "Finn", process.argv is filled with file paths and the string "Finn"
-// then just "Finn" will be console logged, as it was extracted from the other thing
+const [name, github] = profileDataArgs;
+// create 2 variables called user and github
+// fill them with the first two values of profileDataArgs
 
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    // throw means, if there's an error, stop the whole process and display error message
+    // we're specifically making sure it returns an Error object
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+// three arguments:
+// name of file being written
+// data to be written to the file - here, that's the data returned from generatePage()
+// callback function that will run when the file has been written - handling errors and successes
+
+// btw, errors are objects too!! the two main properties of an Error object are the name and message :)
