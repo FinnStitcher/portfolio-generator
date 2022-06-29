7c244ed24ba0fce43d463b260ef207f7e17fb337
Finn Stitcher <foundfootage@protonmail.com>
2022-06-29 15:53:33 -0700

    recursion to add multiple projects

--- app.js
+++ app.js
@@ -1,15 +1,94 @@
 const inquirer = require('inquirer');
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-    }
-])
-// good old fashioned array of objects :)
-.then(answers => console.log(answers));
-// apparently prompt returns a promise, but it doesn't seem like i have to process it into json...
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+};
+// returns a promise
+// it doesn't seem like i have to process it into json... that's very strange
+
+const promptProject = (portfolioData) => {
+    // check if portfolioData contains a property called projects
+    if (!portfolioData.projects) {
+        // there isn't, so let's make one, assigning it an empty array
+        portfolioData.projects = [];
+    };
+
+    console.log(`
+    ==========
+    Add A New Project
+    ==========
+    `);
+
+    // return the end result of this method chain
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project:'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply.)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project:'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddMore',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then((projectData) => {
+        // receive the array returned by .prompt, and put it in portfolioData.projects
+        portfolioData.projects.push(projectData);
+
+        // check if they said yes or no to ading more
+        if (projectData.confirmAddMore) {
+            return promptProject(portfolioData);
+            // run promptProject() all over again, giving it the new value of portfolioData
+        } else {
+            return portfolioData;
+            // portfolioData and its contents will be shuttled up to the top
+        }
+    });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => console.log(portfolioData));
 
 // const fs = require("fs");
 // const generatePage = require("./src/page-template.js");
