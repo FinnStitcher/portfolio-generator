091af5da1179554fb0a2946f2821986c85759771
Finn Stitcher <foundfootage@protonmail.com>
2022-06-30 15:14:28 -0700

    print user data

--- app.js
+++ app.js
@@ -1,152 +1,204 @@
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-const inputError = input => {
-    if (input) {
-        return true;
-    } else {
-        console.log("This information is required.");
-        return false;
-    }
-}
+const generatePage = require("./src/page-template.js");
+
+const dummyData = {
+	name: "Lernantino",
+	github: "lernantino",
+	confirmAbout: true,
+	about: "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.",
+	projects: [
+		{
+			name: "Run Buddy",
+			description:
+				"Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
+			languages: ["HTML", "CSS"],
+			link: "https://github.com/lernantino/run-buddy",
+			feature: true,
+			confirmAddProject: true,
+		},
+		{
+			name: "Taskinator",
+			description:
+				"Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
+			languages: ["JavaScript", "HTML", "CSS"],
+			link: "https://github.com/lernantino/taskinator",
+			feature: true,
+			confirmAddProject: true,
+		},
+		{
+			name: "Taskmaster Pro",
+			description:
+				"Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
+			languages: ["JavaScript", "jQuery", "CSS", "HTML", "Bootstrap"],
+			link: "https://github.com/lernantino/taskmaster-pro",
+			feature: false,
+			confirmAddProject: true,
+		},
+		{
+			name: "Robot Gladiators",
+			description:
+				"Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.",
+			languages: ["JavaScript"],
+			link: "https://github.com/lernantino/robot-gladiators",
+			feature: false,
+			confirmAddProject: false,
+		},
+	],
+};
+
+const inputError = (input) => {
+	if (input) {
+		return true;
+	} else {
+		console.log("This information is required.");
+		return false;
+	}
+};
 
 const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-            validate: input => inputError(input)
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your GitHub username?',
-            validate: input => inputError(input)
-        },
-        {
-            type: "confirm",
-            name: "confirmAbout",
-            message: 'Would you like to enter some information for an "About" section?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself:',
-            when: ({confirmAbout}) => {
-                if (confirmAbout) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-    ]);
+	return inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "What is your name?",
+			validate: (input) => inputError(input),
+		},
+		{
+			type: "input",
+			name: "github",
+			message: "What is your GitHub username?",
+			validate: (input) => inputError(input),
+		},
+		{
+			type: "confirm",
+			name: "confirmAbout",
+			message:
+				'Would you like to enter some information for an "About" section?',
+			default: true,
+		},
+		{
+			type: "input",
+			name: "about",
+			message: "Provide some information about yourself:",
+			when: ({confirmAbout}) => {
+				if (confirmAbout) {
+					return true;
+				} else {
+					return false;
+				}
+			},
+		},
+	]);
 };
 // returns a promise
-// it doesn't seem like i have to process it into json... that's very strange
 
 const promptProject = (portfolioData) => {
-    // check if portfolioData contains a property called projects
-    if (!portfolioData.projects) {
-        // there isn't, so let's make one, assigning it an empty array
-        portfolioData.projects = [];
-    };
+	// check if portfolioData contains a property called projects
+	if (!portfolioData.projects) {
+		// there isn't, so let's make one, assigning it an empty array
+		portfolioData.projects = [];
+	}
 
-    console.log(`
+	console.log(`
     ==========
     Add A New Project
     ==========
     `);
 
-    // return the end result of this method chain
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of your project?',
-            validate: input => inputError(input)
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of the project:',
-            validate: input => inputError(input)
-        },
-        {
-            type: 'checkbox',
-            name: 'languages',
-            message: 'What did you build this project with? (Check all that apply.)',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-        },
-        {
-            type: 'input',
-            name: 'link',
-            message: 'Enter the GitHub link to your project:',
-            validate: input => inputError(input)
-        },
-        {
-            type: 'confirm',
-            name: 'feature',
-            message: 'Would you like to feature this project?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAddMore',
-            message: 'Would you like to enter another project?',
-            default: false
-        }
-    ])
-    .then((projectData) => {
-        // receive the array returned by .prompt, and put it in portfolioData.projects
-        portfolioData.projects.push(projectData);
+	// return the end result of this method chain
+	return inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "name",
+				message: "What is the name of your project?",
+				validate: (input) => inputError(input),
+			},
+			{
+				type: "input",
+				name: "description",
+				message: "Provide a description of the project:",
+				validate: (input) => inputError(input),
+			},
+			{
+				type: "checkbox",
+				name: "languages",
+				message:
+					"What did you build this project with? (Check all that apply.)",
+				choices: [
+					"JavaScript",
+					"HTML",
+					"CSS",
+					"ES6",
+					"jQuery",
+					"Bootstrap",
+					"Node",
+				],
+			},
+			{
+				type: "input",
+				name: "link",
+				message: "Enter the GitHub link to your project:",
+				validate: (input) => inputError(input),
+			},
+			{
+				type: "confirm",
+				name: "feature",
+				message: "Would you like to feature this project?",
+				default: false,
+			},
+			{
+				type: "confirm",
+				name: "confirmAddMore",
+				message: "Would you like to enter another project?",
+				default: false,
+			},
+		])
+		.then((projectData) => {
+			// receive the array returned by .prompt, and put it in portfolioData.projects
+			portfolioData.projects.push(projectData);
 
-        // check if they said yes or no to ading more
-        if (projectData.confirmAddMore) {
-            return promptProject(portfolioData);
-            // run promptProject() all over again, giving it the new value of portfolioData
-        } else {
-            return portfolioData;
-            // portfolioData and its contents will be shuttled up to the top
-        }
-    });
+			// check if they said yes or no to adding more
+			if (projectData.confirmAddMore) {
+				return promptProject(portfolioData);
+				// run promptProject() all over again, giving it the new value of portfolioData
+			} else {
+				return portfolioData;
+				// portfolioData and its contents will be shuttled up to the top
+			}
+		});
 };
 
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => console.log(portfolioData));
+// promptUser()
+//     .then(promptProject)
+//     .then(portfolioData => {
+//         const pageHTML = generatePage(dummyData);
 
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+//         fs.writeFile('./index.html', pageHTML, err => {
+//             if (err) throw new Error(err);
 
-// const pageHTML = generatePage(name, github);
+//             console.log("Page created. Check out index.html to see the output.")
+//         })
+//         // three arguments:
+//         // name of file being written
+//         // data to be written to the file - here, that's the data returned from generatePage()
+//         // callback function that will run when the file has been written - handling errors and successes
+//     });
 
-// fs.writeFile("./index.html", pageHTML, err => {
-//     if (err) throw err;
+const dummyFunction = () => {
+	const pageHTML = generatePage(dummyData);
 
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+	fs.writeFile("./index.html", pageHTML, (err) => {
+		if (err) throw new Error(err);
 
+		console.log("Page created. Check out index.html to see the output.");
+	});
+	// three arguments:
+	// name of file being written
+	// data to be written to the file - here, that's the data returned from generatePage()
+	// callback function that will run when the file has been written - handling errors and successes
+};
 
-/* OLD CODE */
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// // take everything from index 2 to the end and put it in profileDatArgs
-// // if i run node app.js "Finn", process.argv is filled with file paths and the string "Finn"
-// const [name, github] = profileDataArgs;
-// // create 2 variables called user and github
-// // fill them with the first two values of profileDataArgs
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw new Error(err);
-//     // throw means, if there's an error, stop the whole process and display error message
-//     // we're specifically making sure it returns an Error object
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-// // three arguments:
-// // name of file being written
-// // data to be written to the file - here, that's the data returned from generatePage()
-// // callback function that will run when the file has been written - handling errors and successes
-
-// // btw, errors are objects too!! the two main properties of an Error object are the name and message :)
+dummyFunction();
